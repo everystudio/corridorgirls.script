58630827d4b9f4fa019929c60cd73c1a9b45c883
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-01-02 22:46:27 +0900

    ミッション表示とかするところを追加準備

---
+++ game/PanelMission.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PanelMission : MonoBehaviour {
+
+	public TextMeshProUGUI m_txtMain;
+
+	public Button m_btnYes;
+	public Button m_btnNo;
+
+	public Button m_btnContinue;
+
+
+}
