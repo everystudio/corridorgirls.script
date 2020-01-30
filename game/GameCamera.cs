3cd693f204f9f4a1037fadd2e79fcb2f2111bfd7
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-01-30 19:13:32 +0900

    カメラの制御追加

--- game/GameCamera.cs
+++ game/GameCamera.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using HedgehogTeam;
+
 public class GameCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Vector3 offset;
+
+	public float scale;
+
+	public CharaControl m_charaControl;
+	public Camera m_Camera;
+
+	public void MoveCompleteHandler()
+	{
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void ResetPosition()
+	{
+		iTween.MoveTo(m_Camera.gameObject,
+				iTween.Hash(
+					"x", offset.x,
+					"y", offset.y,
+					"z", offset.z,
+					"time", 0.5f,
+					"oncomplete", "MoveCompleteHandler",
+					"oncompletetarget", gameObject,
+					"isLocal", true)
+					);
+
 	}
+
+	public void OnSwipe(HedgehogTeam.EasyTouch.Gesture _a)
+	{
+		Debug.Log(string.Format("x={0} y={1}", _a.deltaPosition.x, _a.deltaPosition.y));
+
+		m_Camera.transform.localPosition += new Vector3(_a.deltaPosition.x * -1.0f, _a.deltaPosition.y * -1.0f, 0.0f) * scale;
+
+	}
+
 }
